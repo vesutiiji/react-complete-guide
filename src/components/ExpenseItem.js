8aacbb9e64b074c513dbfac118ac6c1d7a20784b
vesutiiji <vesutiiji@gmail.com>
2021-04-15 19:03:16 -0400

    removed hard coded data in ExpenseItem.js divs and assigned const in place of the hard coded data.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 4, 15);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (<div>
-                <div className="expense-item">April 15th 2021</div>
+                <div className="expense-item">{expenseDate.toISOString()}</div>
                 <div className="expense-item__description">
-                    <h2>Car Insurance</h2>
-                    <div className="expense-item__price">$294.67</div>
+                    <h2>{expenseTitle}</h2>
+                    <div className="expense-item__price">${expenseAmount}</div>
                 </div>
             </div>
     );
