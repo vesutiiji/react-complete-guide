5af56099ecb529e402f7c595f9a2c1bb44f98e19
vesutiiji <vesutiiji@gmail.com>
2021-05-28 01:46:42 -0400

    cleaned Expenses.js jsx snippet by utilizing if statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,20 @@ function Expenses(props) {
   const filteredExpenses = props.arr.filter((expense) => {
     return expense.date.getFullYear().toString() === filterState;
   });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -19,14 +33,7 @@ function Expenses(props) {
           defaultYear={filterState}
           yearSelectHandler={yearSelectHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
