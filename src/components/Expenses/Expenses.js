4b66a19fdb8f18214028ba2f372484fea4693517
vesutiiji <vesutiiji@gmail.com>
2021-05-28 22:32:50 -0400

    added Chart.js and ChartBar.js to finish project. added dynamic styling

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filterState, setFilterState] = useState("2020");
@@ -20,6 +21,7 @@ const Expenses = (props) => {
           defaultYear={filterState}
           yearSelectHandler={yearSelectHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
